d2db97c65f2a8fbbad14131142a278b7a3fe6178
Eclud300 <eclud673@gmail.com>
2025-04-07 14:34:41 -0700

    intentando que jale circuito

--- View/Inicio.xaml.cs
+++ View/Inicio.xaml.cs
@@ -1,10 +1,37 @@
+using ProyectoBomba.ViewModel;
+using System.Windows.Input;
+
 namespace ProyectoBomba.View
 {
     public partial class Inicio : ContentPage
     {
+        private readonly InicioViewModel viewModel;
+
         public Inicio()
         {
             InitializeComponent();
+            viewModel = new InicioViewModel();
+            BindingContext = viewModel;
+        }
+
+        private async void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            await viewModel.ToggleBombaCommand.ExecuteAsync(e.Value);
+        }
+    }
+
+    // Extensión para ejecutar comandos asíncronos con parámetros
+    public static class CommandExtensions
+    {
+        public static Task ExecuteAsync<T>(this ICommand command, T parameter)
+        {
+            var taskCompletionSource = new TaskCompletionSource<object>();
+            if (command is Command<T> cmd && cmd.CanExecute(parameter))
+            {
+                cmd.Execute(parameter);
+            }
+            taskCompletionSource.SetResult(null);
+            return taskCompletionSource.Task;
         }
     }
 }
---
+++ ViewModel/InicioViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProyectoBomba.ViewModel
+{
+    internal class InicioViewModel
+    {
+        private readonly HttpClient _httpClient; 
+        private const string BombaApiEncenderUrl = "http://172.16.28.150/encenderbomba";
+        private const string BombaApiApagarUrl = "http://172.16.28.150/apagarbomba";
+
+        public ICommand ToggleBombaCommand { get; }
+
+        public bool IsBombaEncendida { get; set; }
+
+        public InicioViewModel()
+        {
+            _httpClient = new HttpClient();
+            ToggleBombaCommand = new Command<bool>(async (isOn) => await CambiarEstadoBomba(isOn));
+        }
+        
+
+        private async Task CambiarEstadoBomba(bool encender)
+        {
+            try
+            {
+                var url = encender ? BombaApiEncenderUrl : BombaApiApagarUrl;
+                var response = await _httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var mensaje = encender ? "🚿 Bomba encendida" : "💧 Bomba apagada";
+                    await Application.Current.MainPage.DisplayAlert("Bomba", mensaje, "OK");
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "No se pudo cambiar el estado de la bomba", "OK");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", $"Error de conexión: {ex.Message}", "OK");
+            }
+           catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", $"Ocurrió un error: {ex.Message}", "OK");
+            }
+        }
+    }
+}
